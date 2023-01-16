fd9e1f425af8fe45954dbd8a1afd82a6c7cde7ca
Gabriel <gabrieldubois.eng@gmail.com>
2023-01-16 11:51:23 -0500

    unselectable text

--- components/Marquee.js
+++ components/Marquee.js
@@ -4,14 +4,14 @@ const Marquee = ({ title, color }) => {
     return (
         <div className={`marquee border-b-2 border-t-2 ${color}`}>
             <div className="marquee-content scroll">
-                <h1>{title}</h1>
-                <h1>{title}</h1>
-                <h1>{title}</h1>
+                <h1 className="unselectable">{title}</h1>
+                <h1 className="unselectable">{title}</h1>
+                <h1 className="unselectable">{title}</h1>
             </div>
             <div className="marquee-content scroll">
-                <h1>{title}</h1>
-                <h1>{title}</h1>
-                <h1>{title}</h1>
+                <h1 className="unselectable">{title}</h1>
+                <h1 className="unselectable">{title}</h1>
+                <h1 className="unselectable">{title}</h1>
             </div>
         </div>
     )
