602b47a10ff22a9be7b4be0b50af9bab4bc2862a
Gabriel <gabrieldubois.eng@gmail.com>
2023-01-16 19:43:38 -0500

    wow

--- pages/index.js
+++ pages/index.js
@@ -7,12 +7,15 @@ import Marquee from '@/components/Marquee'
 export default function Home() {
 
   const [color, setColor] = useState("red");
+  const [title, setTitle] = useState("Software Engineer")
   const [currentIndex, setCurrentIndex] = useState(0);
   const colors = ["red", "green", "blue", "purple"];
+  const titles = ["Software Engineer", "Student", "Game Programmer", "Template"];
 
   function changeColor(newIndex) {
     setCurrentIndex(newIndex);
     setColor(colors[newIndex]);
+    setTitle(titles[newIndex]);
   }
 
   return (
@@ -20,10 +23,10 @@ export default function Home() {
       <Layout>
 
         <div className="flex justify-center">
-          <ArrowLeftCircleIcon className='w-10 cursor-pointer' onClick={() => changeColor((currentIndex - 1 + colors.length) % colors.length)}> Previous</ArrowLeftCircleIcon>
-          <ArrowRightCircleIcon className='w-10 cursor-pointer' onClick={() => changeColor((currentIndex + 1) % colors.length)}>Next</ArrowRightCircleIcon>
+          <ArrowLeftCircleIcon className={`w-10 cursor-pointer ${color}`} onClick={() => changeColor((currentIndex - 1 + colors.length) % colors.length)}> Previous</ArrowLeftCircleIcon>
+          <ArrowRightCircleIcon className={`w-10 cursor-pointer ${color}`} onClick={() => changeColor((currentIndex + 1) % colors.length)}>Next</ArrowRightCircleIcon>
         </div>
-        <Marquee color={color} title="G@mer"></Marquee>
+        <Marquee color={color} title={title}></Marquee>
       </Layout>
     </>
   )
