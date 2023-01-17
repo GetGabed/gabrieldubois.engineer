a7b25beab54a90bb39c9ac8e3ef2b80da8085944
Gabriel <gabrieldubois.eng@gmail.com>
2023-01-16 23:16:47 -0500

    looking good

---
+++ components/Hero.js
@@ -0,0 +1,15 @@
+const Hero = ({ title, color }) => {
+
+    return (
+        <>
+
+
+            <div className="flex items-center">
+                <h1 className="hero-p text-5xl mr-2 text-white dark:text-black">and I'm a</h1>
+                <h1 className={`${color} hero-txt`}>{title}</h1>
+            </div>
+        </>
+    )
+}
+
+export default Hero;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Head from 'next/head'
 import { useState, useEffect } from 'react'
 import Layout from '@/components/Layout'
-import { ArrowLeftCircleIcon, ArrowRightCircleIcon } from '@heroicons/react/24/outline'
+import { ArrowLeftCircleIcon, ArrowRightCircleIcon, ArrowUturnRightIcon } from '@heroicons/react/24/outline'
 import Marquee from '@/components/Marquee'
+import Hero from '@/components/Hero'
 
 export default function Home() {
 
@@ -10,7 +11,7 @@ export default function Home() {
   const [title, setTitle] = useState("Software Engineer")
   const [currentIndex, setCurrentIndex] = useState(0);
   const colors = ["red", "green", "blue", "purple"];
-  const titles = ["Software Engineer", "Student", "Game Programmer", "Template"];
+  const titles = ["Software Engineer", "Student", "Game Programmer", "Dreamer"];
 
   function changeColor(newIndex) {
     setCurrentIndex(newIndex);
@@ -22,9 +23,20 @@ export default function Home() {
     <>
       <Layout>
 
-        <div className="flex justify-center">
-          <ArrowLeftCircleIcon className={`w-10 cursor-pointer ${color}`} onClick={() => changeColor((currentIndex - 1 + colors.length) % colors.length)}> Previous</ArrowLeftCircleIcon>
-          <ArrowRightCircleIcon className={`w-10 cursor-pointer ${color}`} onClick={() => changeColor((currentIndex + 1) % colors.length)}>Next</ArrowRightCircleIcon>
+        <div className="flex flex-col items-center hero">
+
+          <h1 className="hero-p text-6xl text-current">Hello, the name's Gabriel</h1>
+
+          <div className="flex">
+            <ArrowLeftCircleIcon className={`w-10 cursor-pointer ${color}`} onClick={() => changeColor((currentIndex - 1 + colors.length) % colors.length)} />
+            <Hero title={title} color={color} />
+            <ArrowRightCircleIcon className={`w-10 cursor-pointer ${color}`} onClick={() => changeColor((currentIndex + 1) % colors.length)} />
+          </div>
+          <div className="mt-1 flex items-center hero-link hover:border-b border-current">
+            <ArrowUturnRightIcon className='w-6 mr-2'></ArrowUturnRightIcon>
+            <h1 className='text-2xl'>Learn more about the product that I am</h1>
+          </div>
+
         </div>
         <Marquee color={color} title={title}></Marquee>
       </Layout>
