2c790c5233d62b219cc243b8c394f10aa69aebf2
Gabriel <gabrieldubois.eng@gmail.com>
2023-01-22 15:06:13 -0500

    ortograph

--- pages/about.js
+++ pages/about.js
@@ -26,7 +26,7 @@ const About = () => {
                     <div className="flex">
                         <div>
                             <p className="ml-16 mr-12 text-lg mb-12">Hello! My name is Gabriel and I take     pleasure in developing features that will be used and appreciated by a handful of users. My passion for programming started in the mid 2010's as I was trying to understand how the games and websites I've been exploring were made.<br /><br />
-                                Since these days, I've had the opportunity to participate to many <Link href='/experience' className={`${color} hover:border-b border-current`}>hackathons</Link>, work at a <Link href="https://www.ibwave.com" target="_blank" className={`${color} hover:border-b border-current`}>wireless network global leader</Link>, and create meaningful <Link href='/work' className={`${color} hover:border-b border-current`}>projects</Link> that improved my skills.
+                                Since these days, I've had the opportunity to participate in many <Link href='/experience' className={`${color} hover:border-b border-current`}>hackathons</Link>, work at a <Link href="https://www.ibwave.com" target="_blank" className={`${color} hover:border-b border-current`}>wireless network global leader</Link>, and create meaningful <Link href='/work' className={`${color} hover:border-b border-current`}>projects</Link> that improved my skills.
                             </p>
                             <hr className={`${color} ml-4 h-px w-1/2`}></hr>
                         </div>
