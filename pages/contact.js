594be448a675467a1e248fa65a05910b21bf5111
Gabriel <gabrieldubois.eng@gmail.com>
2023-01-27 11:43:22 -0500

    fast email

--- pages/contact.js
+++ pages/contact.js
@@ -1,5 +1,6 @@
 const { default: Layout } = require("@/components/Layout")
 import { useEffect, useState } from "react";
+import Link from "next/link";
 
 const Contact = () => {
     const [color, setColor] = useState("red");
@@ -19,6 +20,7 @@ const Contact = () => {
                 <h1 className="sf-mono ml-4 font-bold text-2xl">Contact</h1>
                 <hr className={`${color} ml-4 h-px w-1/2`}></hr>
             </div>
+            <h1 className="mx-12"> To contact me, please go throught my <Link className={`${color} hover:border-b border-current`} href="https://www.linkedin.com/in/gabriel-dubois-soen/">LinkedIn</Link> or <Link className={`${color} hover:border-b border-current`} href = "mailto: gabrieldubois.eng@gmail.com">my email: gabrieldubois.eng@gmail.com</Link></h1>
         </Layout>
         </>
     )
