202fef38b9678f07c5959bf83a13bf93613f1b04
Gabriel <gabrieldubois.eng@gmail.com>
2023-01-30 01:14:00 -0500

    gotta go sleep

--- components/projects/MainProject.js
+++ components/projects/MainProject.js
@@ -1,5 +1,8 @@
 import Image from "next/image";
-const MainProject = ({image, title, description, github, link, techs, color}) => {
+import Link from "next/link";
+import { ArrowUpRightIcon } from "@heroicons/react/24/outline";
+
+const MainProject = ({ image, title, description, github, link = "work", techs, color, reversed = false }) => {
 
     const showAllTechs = () => {
         return techs.map((t, i) => (
@@ -10,20 +13,68 @@ const MainProject = ({image, title, description, github, link, techs, color}) =>
     };
 
     return (
-        <><div className="flex justify-center items-center">
-            <div className="clash-display ml-36 z-10">
-                <div className="">
-                    <h1 className={`w-max text-2xl font-medium ${color}`}>{title}</h1>
-                    <ul className={`flex w-max mb-4  font-normal`}>
-                        {showAllTechs()}
-                    </ul>
-                    <p className="bg-black-100 dark:bg-gray-900 text-white rounded-xl  p-4 w-96">{description}</p>
-                </div>
-                
-            </div>
-            <div className="flex justify-center z-0 -ml-8">
-                <Image src={image} width={500} height={500} className="object-contain mr-12 rounded-2xl " />
-            </div>
+        <><div className="mt-12 flex justify-center items-center">
+            {reversed ? (
+                <>
+                    <div className="flex justify-center -ml-8">
+                        <Link href={link} className={link == 'work' ? "cursor-default z-0 " : 'z-0'}>
+                            <Image src={image} width={700} height={700} className="object-contain projpic duration-500 -mr-12 rounded-2xl " />
+                        </Link>
+                    </div>
+                    <div className="clash-display -ml-12 z-10">
+                        <div>
+                            <h1 className={` text-2xl font-medium text-right ${color}`}>{title}</h1>
+                            <ul className={`flex justify-end -mr-2 mb-4 font-normal`}>
+                                {showAllTechs()}
+                            </ul>
+                            <div className="bg-black-100 dark:bg-gray-900 w-96 pt-4 px-4 pb-2 rounded-xl shadow-xl">
+                                <p className="text-white mb-4 leading-relaxed">{description} </p>
+                                <div className="flex flex-1 justify-end items-center ">
+                                    {github ? (
+                                        <Link href={github} className="hover:mr-1 duration-500" >
+                                            <i className={`devicon-github-original text-3xl hover:text-white  transition-colors ml-4 ${color}`} />
+                                        </Link>
+                                    ) : ""}
+                                    {link ? (
+                                        <Link href={link} className="hover:-mt-2 duration-500" target="_blank">
+                                            <ArrowUpRightIcon className={`border-b border-solid hover:text-white hover:border-white transition-colors ml-4 w-7 ${color}`} />
+                                        </Link>
+                                    ) : ""}
+                                </div>
+                            </div>
+                        </div>
+                    </div></>) :
+                (<>
+                    <div className="clash-display ml-36 z-10">
+                        <div>
+                            <h1 className={`w-max text-2xl font-medium ${color}`}>{title}</h1>
+                            <ul className={`flex w-max mb-4 font-normal`}>
+                                {showAllTechs()}
+                            </ul>
+                            <div className="bg-black-100 dark:bg-gray-900 w-96 pt-4 px-4 pb-2 rounded-xl shadow-xl">
+                                <p className="text-white mb-4 leading-relaxed">{description} </p>
+                                <div className="flex flex-1 justify-start items-center">
+                                    {github ? (
+                                        <Link href={github} className="hover:ml-1 duration-500" >
+                                            <i className={`devicon-github-original text-3xl hover:text-white  transition-colors ml-4 ${color}`} />
+                                        </Link>
+                                    ) : ""}
+                                    {link != "work" ? (
+                                        <Link href={link} className="hover:-mt-2 duration-500" target="_blank">
+                                            <ArrowUpRightIcon className={`border-b border-solid hover:text-white hover:border-white transition-colors ml-4 w-7 ${color}`} />
+                                        </Link>
+                                    ) : ""}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex justify-center z-0 -ml-8">
+                        <Link href={link} className={link == 'work' ? "cursor-default" : ''}>
+                            <Image src={image} width={700} height={700} className="object-contain mr-12 rounded-2xl " />
+                        </Link>
+
+                    </div>
+                </>)}
         </div></>
     )
 }
--- pages/work.js
+++ pages/work.js
@@ -4,29 +4,41 @@ import MainProject from "@/components/projects/MainProject";
 
 const Work = () => {
     const [color, setColor] = useState("red");
-   
-        
-    useEffect(()=> {
+
+
+    useEffect(() => {
         const storedColor = localStorage.getItem('color');
         if (storedColor) {
-          setColor(JSON.parse(storedColor));
+            setColor(JSON.parse(storedColor));
         }
     })
     return (
         <>
-        <Layout color={color}>
-            <div className="flex items-center text-xl mx-12 mb-12">
-                <h1 className={`${color} sf-mono`}>03.</h1>
-                <h1 className="sf-mono ml-4 font-bold text-2xl">Projects that I've worked on</h1>
-                <hr className={`${color} ml-4 h-px w-1/2`}></hr>
-            </div>
-            <MainProject 
-            image="/projects/CoursesDB.png" 
-            title="CoursesDB"
-            techs={["VS Code", "NextJS", "MongoDB", "NodeJS"]}
-            description="A fullstack application created for the purpose of giving the opportunity to students to share and read summaries of courses they are or will be taking. Everyone can create an account, and when given the right, can also write a summary."
-            color={color}/>
-        </Layout>
+            <Layout color={color}>
+                <div className="flex items-center text-xl mx-12 ">
+                    <h1 className={`${color} sf-mono`}>03.</h1>
+                    <h1 className="sf-mono ml-4 font-bold text-2xl">Projects I've worked on</h1>
+                    <hr className={`${color} ml-4 h-px w-1/2`}></hr>
+                </div>
+                <MainProject
+                    image="/projects/CoursesDB.png"
+                    title="CoursesDB"
+                    techs={["NextJS", "MongoDB", "Express", "NodeJS"]}
+                    description="A fullstack application created for the purpose of giving the opportunity to students to share and read summaries of courses they are or will be taking. Everyone can create an account, and when given the right, can also write a post."
+                    color={color}
+                    github="https://github.com/Adissuu/CoursesDB"
+                />
+                <MainProject
+                    image="/projects/pokedex.png"
+                    title="PokéAPI"
+                    techs={["React", "Axios", "Router", "Framer motion"]}
+                    description="A project that a huge amount of developers works on at one point or another. This time, I had the chance to greatly learn about Axios and everything related to API calls."
+                    color={color}
+                    github="https://github.com/Adissuu/Pokedex-api"
+                    link="https://adissuu.github.io/Pokedex-api/#/"
+                    reversed={true}
+                />
+            </Layout>
         </>
     )
 }
