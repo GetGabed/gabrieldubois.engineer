24ce6fb92cec6c9f068a91da2f1816bc574c34fc
Gabriel Dubois <gabrieldubois.eng@gmail.com>
2025-03-26 22:13:05 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
   basePath: '/adissuu.github.io/gabrieldubois.engineer',
   assetPrefix: '/adissuu.github.io/gabrieldubois.engineer',
 };
