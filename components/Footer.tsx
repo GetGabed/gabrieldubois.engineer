7b943ada562b0e928d464d9701883b89c996c5db
Gabriel Dubois <gabrieldubois.eng@gmail.com>
2026-02-16 22:51:36 -0500

    new year

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <div className='mt-12'>
-        <h1 className='text-sm text-bold tracking-widest mb-4 text-gray-500'>©Gabriel Dubois {new Date().getFullYear()}.</h1>
+      <h1 className='text-sm text-bold tracking-widest mb-4 text-gray-500'>©Gabriel Dubois {new Date().getFullYear()}.</h1>
     </div>
   )
 }
--- components/ui/timeline-layout.tsx
+++ components/ui/timeline-layout.tsx
@@ -19,16 +19,26 @@ type TimelineItemType = {
 const timelineData: TimelineItemType[] = [
   {
     id: 1,
-    title: 'Shopify - Software Engineer Intern',
+    title: 'Shopify - Software Engineer',
     description: `
-        Migrated internal monitoring from Bugsnag to Grafana, reducing alert latency by 40% and improving alert customization. Engineered trust score recording system to track merchant integrity for each action, enabling data-informed enforcement and risk assessment across millions of shops.
-    `,    
-    time: 'May 2025 – Aug. 2025',
+        Making online commerce trustworthy.
+    `,
+    time: 'Jan. 2026 – Present',
     link: "https://www.shopify.com",
     icon: "/gabrieldubois.engineer/svg/shopify_glyph.svg",
   },
   {
     id: 2,
+    title: 'Shopify - Software Engineer Intern',
+    description: `
+        Migrated internal monitoring from Bugsnag to Grafana, reducing alert latency by 40% and improving alert customization. Engineered trust score recording system to track merchant integrity for each action, enabling data-informed enforcement and risk assessment across millions of shops.
+    `,
+    time: 'May 2025 – Aug. 2025',
+    link: "https://www.shopify.com",
+    icon: "/gabrieldubois.engineer/svg/shopify_glyph.svg",
+  },
+  {
+    id: 3,
     title: 'Ubisoft - Software Engineer Intern',
     description: `
         Developed high-performance online systems, enhancing player notifications, connections, and asset management for millions of users. Monitored AWS instances with Grafana, Splunk, and ElasticSearch. Optimized CI/CD pipeline by 30% through parallelization and caching, improving code maturity with SonarQube and Renovate.
@@ -38,16 +48,16 @@ const timelineData: TimelineItemType[] = [
     icon: "/gabrieldubois.engineer/svg/ubisoft.svg",
   },
   {
-    id: 3,
+    id: 4,
     title: 'Genetec - Software Engineer Intern',
-    description:`
+    description: `
         Implemented a multithreaded feature for faster Modbus device discovery, reducing setup time by 10x. Added plugin factories for XML integration and regex substitutions, expanding software capabilities. Optimized plugin responsiveness, improving data serialization and reducing device polling intervals by 60%.
     `,
     time: 'May 2023 – Sep. 2023',
     link: "https://www.genetec.com",
   },
   {
-    id: 4,
+    id: 5,
     title: 'iBwave - Software Engineer Intern',
     description: `
         Addressed security concerns with front-end and back-end verifications, ensuring data integrity during project deletions. Increased test coverage with comprehensive unit, integration, and automated tests using Selenium and SQL.
@@ -67,22 +77,22 @@ export const TimelineLayout = () => {
             <TimelineTitle href={item.link}>
               {item.title}
               {item.icon && (
-                <img 
-                src={item.icon} 
-                alt={`${item.title} icon`}
-                className="inline-block ml-2 mb-2 w-6 h-6" 
+                <img
+                  src={item.icon}
+                  alt={`${item.title} icon`}
+                  className="inline-block ml-2 mb-2 w-6 h-6"
                 />
               )}
             </TimelineTitle>
           </TimelineHeader>
           <TimelineDescription>{item.description
-              .trim()
-              .split('\n')
-              .map((line, index) => (
-                <p key={index} className="mb-2">
-                  {line.trim()}
-                </p>
-              ))}</TimelineDescription>
+            .trim()
+            .split('\n')
+            .map((line, index) => (
+              <p key={index} className="mb-2">
+                {line.trim()}
+              </p>
+            ))}</TimelineDescription>
         </TimelineItem>
       ))}
     </Timeline>
